f64af55889ae5a599a3b3215e04391d6aa1248ce
ogutuderick <ogutuderick4@gmail.com>
2023-08-15 20:34:30 +0300

    My privileges!

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,17 @@
---list all priviledges
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+-- This script creates MySQL users and grants privileges to them.
+-- Make sure to have MySQL server running on localhost.
+
+-- Create user user_0d_1
+CREATE USER 'user_0d_1'@'localhost';
+
+-- Grant all privileges to user_0d_1
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+
+-- Create user user_0d_2
+CREATE USER 'user_0d_2'@'localhost';
+
+-- Grant all privileges to user_0d_2
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost';
+
+-- Flush privileges to apply changes
+FLUSH PRIVILEGES;
