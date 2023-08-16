23f49af1adf41af9b2310cdb959eb2efa30ee974
ogutuderick <ogutuderick4@gmail.com>
2023-08-16 15:03:16 +0300

    My privileges!

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,7 @@
---list all priviledges
-SHOW GRANTS IF EXISTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS IF EXISTS FOR 'user_0d_2'@'localhost';
+-- List privileges of MySQL users user_0d_1 and user_0d_2 on the server (localhost)
+
+-- List privileges for user_0d_1
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- List privileges for user_0d_2
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
