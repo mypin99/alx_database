30e779b0ac80c7be8759dd8ca8dc54b6e044e531
ogutuderick <ogutuderick4@gmail.com>
2023-08-16 15:20:16 +0300

    Always a name

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,4 +1,13 @@
---creating table
-CREATE TABLE force_name(
-id INT,
-name VARCHAR(256) NOT NULL);
+-- Create the table force_name on the MySQL server
+-- force_name description:
+-- id INT
+-- name VARCHAR(256) can't be null
+
+-- The database name will be passed as an argument of the mysql command
+-- If the table force_name already exists, your script should not fail
+
+-- Create the table force_name if it doesn't exist
+CREATE TABLE IF NOT EXISTS force_name (
+    id INT PRIMARY KEY,
+    name VARCHAR(256) NOT NULL
+);
