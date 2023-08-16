01a9c9bbd1af0624a3c40fa6c1c4720bf06a7fff
ogutuderick <ogutuderick4@gmail.com>
2023-08-16 14:34:39 +0300

    My privileges!

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,8 +1,3 @@
--- This script checks the existence of MySQL users and provides output accordingly.
--- Make sure to have MySQL server running on localhost.
-
--- Check if user user_0d_1 exists
-SELECT IF(EXISTS(SELECT 1 FROM mysql.user WHERE User = 'user_0d_1'), 'user_0d_1 EXISTS', 'user_0d_1 DOES NOT EXIST') AS Result;
-
--- Check if user user_0d_2 exists
-SELECT IF(EXISTS(SELECT 1 FROM mysql.user WHERE User = 'user_0d_2'), 'user_0d_2 EXISTS', 'user_0d_2 DOES NOT EXIST') AS Result;
+--list all priviledges
+SHOW GRANTS IF EXISTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS IF EXISTS FOR 'user_0d_2'@'localhost';
