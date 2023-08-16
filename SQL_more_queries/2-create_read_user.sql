f7ce0a56ca278d7baa7f80e84360a0d4f9878766
ogutuderick <ogutuderick4@gmail.com>
2023-08-16 15:10:30 +0300

    Read user

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,8 +1,19 @@
---creating user
-CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY user_0d_1_pwd;
+-- Create database hbtn_0d_2 and user user_0d_2
+-- user_0d_2 should have only SELECT privilege in the database hbtn_0d_2
+-- The user_0d_2 password should be set to user_0d_2_pwd
 
---creating database
-CREATE SCHEMA hbtn_0d_2
+-- If the database hbtn_0d_2 already exists, your script should not fail
+-- If the user user_0d_2 already exists, your script should not fail
 
---granting the user all priviledges to the server
-GRANT SELECT ON 'hbtn_0d_2'.* TO 'user_0d_1'@'localhost';
+-- Connect to the MySQL server as root
+-- Note: Make sure to replace 'your_root_password' with your actual root password
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+
+-- Create or update user_0d_2 with the specified password
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+
+-- Grant SELECT privilege to user_0d_2 on the hbtn_0d_2 database
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+
+-- Flush privileges to apply changes
+FLUSH PRIVILEGES;
