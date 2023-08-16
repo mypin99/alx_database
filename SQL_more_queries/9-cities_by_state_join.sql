f672b5d30b8e215364a7fd0fe0326593a015d528
ogutuderick <ogutuderick4@gmail.com>
2023-08-16 16:04:55 +0300

    Cities by States

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,10 @@
-DESCRIBE DATABASE hbtn_0d_usa
+-- List all cities contained in the database hbtn_0d_usa along with their corresponding states
+-- Each record should display: cities.id - cities.name - states.name
+-- Results must be sorted in ascending order by cities.id
+-- The database name will be passed as an argument of the mysql command
+
+-- Query to select cities along with their corresponding states and sort by cities.id
 SELECT cities.id, cities.name, states.name
-FROM hbtn_0d_usa
-WHERE cities.id ASC;
+FROM cities
+JOIN states ON cities.state_id = states.id
+ORDER BY cities.id;
